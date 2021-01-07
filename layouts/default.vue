cc66ca178f65a3fc99df75af96a9f446e244367b
Joshua Galit <joshuaimalay@gmail.com>
2021-01-07 10:29:49 +0800

    import: NavBar vue component setup: default bg-color and text-color nested Nuxt with main div

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="bg-gray-200 font-sans antialiased text-gray-900 min-h-screen h-full">
+    <NavBar />
+    <main>
+      <Nuxt />
+    </main>
   </div>
 </template>
+
+<script>
+  export default {
+    components: {
+      NavBar: () => import('@/components/NavBar')
+    }
+  }
+</script>
