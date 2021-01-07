b3445f4dbb8a34f4b914a51d3fd11f445fe47cbd
Joshua Galit <joshuaimalay@gmail.com>
2021-01-07 11:31:27 +0800

    design: resolute header section - DONE

---
+++ components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="flex items-center bg-blue-resolute w-full">
+      <div class="w-left flex-none p-3 flex flex-col text-white">
+        <span class="text-xl font-semibold tracking-wide leading-tight">Resolute</span>
+        <span class="text-xs uppercase tracking-widest">property tax solutions</span>
+      </div>
+      <div class="relative flex-1 pr-4">
+        <div class="absolute inset-y-0 left-0 flex items-center">
+          <svg class="w-10 h-10 text-gray-600 pl-3" fill="currentColor" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" preserveAspectRatio="xMidYMid meet"><path xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+        </div>
+        <input  class="w-full py-3 px-4 pl-12 text-sm bg-gray-100 border border-blue-resolute-dark outline-0 rounded-sm focus:outline-none" type="text" placeholder="Search by Email, Property Address, PID, Phone Number, or Contract Name">
+      </div>
+    </div>
+  </header>
+</template>
