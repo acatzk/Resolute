ae54b9c50c3b4c97873a948303789113137bd69f
Joshua Galit <joshuaimalay@gmail.com>
2021-01-08 09:53:48 +0800

    Design card details and add slot scoped and props

---
+++ components/CardDetails.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="border border-gray-400 rounded w-full bg-white overflow-hidden">
+    <div :class="`${borderColor} px-4 py-5`">
+      <div class="flex space-x-3">
+        <div class="w-1/3">
+          <slot name="card-header"></slot>
+        </div>
+        <div class="w-2/3">
+          <slot name="card-content"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      borderColor: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
